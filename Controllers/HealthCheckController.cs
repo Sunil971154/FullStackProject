7b8d81e4273199499cd75c6b412f44bb5fb8e4ad
Sunil-kumar <sunilku.951975@gmail.com>
2025-06-24 14:26:12 +0530

    first commit without persistense

---
+++ Controllers/HealthCheckController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Revision_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult HealthCheck()
+        {
+            return Ok("Ok--> http://localhost:5172");
+        }
+
+
+    }
+}
---
+++ Models/JournalEntry.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Revision_Project.Models
+{
+    public class JournalEntry
+    {
+        public long Id { get; set; }      // case matter nahi karta letter chota bada ho dakta hai       
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+
+    }
+}
